ccff815ef6afaf87675ce5a192b3598141378d93
Diego Martinez Alaminos <diegomartinezalaminos@gmail.com>
2019-10-15 13:32:26 +0200

    Ejercicios05

--- Ejercicios05/Ejercicio12.java
+++ Ejercicios05/Ejercicio12.java
@@ -1,13 +1,19 @@
 public class Ejercicio12{
 	public static void main(String [] args){
-		System.out.println("Introduce un número: ");
-		int contador = Integer.parseInt(System.console().readLine());
-		int cont = 1;
+		System.out.println("Introduce un número de filas ");
+		int filas = Integer.parseInt(System.console().readLine());
+		int numero1 = 0;
+		int numero2 = numero1 + 1;
+		int contador = 1;
+		int suma = 0;
+		System.out.print(numero1 + " , " + numero2);
 		
-		while (contadora <= 10){
-			if (){
-				System.out.println(contador + " , " + (contador + 1));
-			}
+		while (contador <= filas){
+			suma = numero1 + numero2;
+			System.out.print(" , " + suma);
+			numero1 = suma - numero1;
+			numero2 = suma;
+			contador++;
 		}
 	}
 }
